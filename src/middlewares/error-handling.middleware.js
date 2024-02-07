4687c18215e1ac9f2fd21538af854f6a9adad6d9
jindonglee <dropnowon@gmail.com>
2024-02-07 16:47:34 +0900

    미들웨어 작성

---
+++ src/middlewares/error-handling.middleware.js
@@ -0,0 +1,11 @@
+module.exports = function (err, req, res, next) {
+  // 에러를 출력합니다.
+  console.error(err);
+  if (err.name === "ValidationError") {
+    return res
+      .status(400)
+      .json({ message: "데이터 형식이 올바르지 않습니다." });
+  }
+
+  res.status(500).json({ errorMessage: "서버 내부 에러가 발생했습니다." });
+};
